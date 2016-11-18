b750caad62a6c609ff4f1ef522ceb767f42f4c46
Zacharias Berggren <zache94@hotmail.com>
2016-11-18 16:00:36 +0100

    Changed to default config credentials

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {};
 
 config.port = 3000;
 
-config.databaseUsername = 'pingu';
-config.databasePassword = '$=RqN5xg';
-config.databaseHost = '192.168.1.9';
+config.databaseUsername = 'user';
+config.databasePassword = 'password';
+config.databaseHost = 'localhost';
 
 
 module.exports = config;
